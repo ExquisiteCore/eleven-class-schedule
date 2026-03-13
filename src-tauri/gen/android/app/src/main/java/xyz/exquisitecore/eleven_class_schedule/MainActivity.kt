9da0fb513039b24616e0fbe21559ef392fad653e
ExquisiteCore <xiaolongqq1y@163.com>
2026-03-13 19:37:53 +0800

    init

---
+++ src-tauri/gen/android/app/src/main/java/xyz/exquisitecore/eleven_class_schedule/MainActivity.kt
@@ -0,0 +1,11 @@
+package xyz.exquisitecore.eleven_class_schedule
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
